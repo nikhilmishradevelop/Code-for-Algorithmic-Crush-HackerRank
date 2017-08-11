9f6b71107b988477b9d06d1d7be934ab701c7697
Akibbalkhi <akibbalkhiad@gmail.com>
2017-08-11 18:47:55 +0530

    Code
    Code For Algorithmic Crush for C++ HackerRank Question - Data Structures.

---
+++ Algorithmic Crush.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<limits.h>
+#include <stdlib.h>
+
+int main() {
+long n,q,temp=INT_MIN,k;
+int a,b,*p,i,j;
+    scanf("%ld%ld",&n,&q);
+    p=(int *)calloc(n,sizeof(int));
+    for(i=0; i < n; i++)
+    {
+        p[i] = 0;
+    }
+ 
+
+     for(i=0;i<q;i++)
+     {
+         scanf("%d%d%d",&a,&b,&k);
+         
+    for(j=a-1;j<b;j++)
+    {
+        p[j]+=k;
+        if(p[j]>=temp)
+            temp=p[j];
+        
+    }
+     }    
+        
+
+free(p);
+
+    printf("%ld ",temp);
+    
+    
+    return 0;
+}
